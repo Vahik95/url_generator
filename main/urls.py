381dac8b1724d6a181a54dcd0e921d18a7d75c71
Vahe <vahikhovsepyan95@gmail.com>
2018-11-22 14:59:01 +0400

    main page url is modified, now it is working

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     url(r'^hash_url/', views.hash_url, name="hash_url"),
+    url(r'^$', views.hash_url, name="hash_url"),
     url(r'^check/', views.check, name="check"),
     url(r'^hash_url_redirect/(?P<url_id>[0-9]+)/$', views.hash_url_redirect, name="hash_url_redirect"),
 
--- urls/urls.py
+++ urls/urls.py
@@ -5,5 +5,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^main/', include('main.urls')),
+    url(r'^$', include('main.urls')),
 
 ]
